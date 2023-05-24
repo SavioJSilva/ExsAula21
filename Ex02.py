d05c6790177cce9117b96b1e1403ce14509cf13b
SavioJSilva <129877737+SavioJSilva@users.noreply.github.com>
2023-05-24 15:51:23 -0300

    adicionando exercicio 02

---
+++ Ex02.py
@@ -0,0 +1,23 @@
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (Opcional) mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+# Programa Principal
+valor = int(input('Deseja calcular o fatorial de qual valor? '))
+print(fatorial(valor, True))
+#help(fatorial)
--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Exercicios da segunda parte da aula sobre funções em python.
 
 Exercicio 01: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
-Exercício 02:
+Exercício 02: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+Exercício 03:
